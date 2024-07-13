c3015777fe7d7673e27357f17b9f726b3d95572d
unknown <you@example.com>
2024-07-13 01:26:06 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Inter',
+      },
+      boxShadow: {
+        shape: '0px 8px 8px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 2px 2px rgba(0, 0, 0, 0.1), 0px 0px 0px 1px rgba(0, 0, 0, 0.1), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.03), inset 0px 1px 0px rgba(255, 255, 255, 0.03)'
+      },
+      colors: {
+        'line-950': '#1A2E05',
+        'line-300': '#BEF264',
+        'line-400': '#a3e635',
+      },
+      backgroundImage: {
+        pattern: 'url(/bg.png)',
+      },
+    },
+  },
+  plugins: [],
+}
